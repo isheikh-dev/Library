11ed5b149a3587e92d5106f502c430627467c6d2
ishiekh-dev@gmail.com <01957368201a>
2019-09-01 14:23:28 +0200

    add new path to book resource

--- app/Book.php
+++ app/Book.php
@@ -8,4 +8,8 @@ class Book extends Model
 {
     // protected $fillable = ['title', 'author'];
     protected $guarded = [];
+
+    public function path(){
+        return '/books/' . $this->id;
+    }
 }
