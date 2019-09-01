c400bd01d19e09b6140d50ce082f58efde5ad3ca
ishiekh-dev@gmail.com <01957368201a>
2019-09-01 04:25:22 +0200

    upload gitignore to exclude .idea directory

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/books', 'BookController@store');
+Route::patch('/books/{book}', 'BookController@update');
--- tests/Feature/BookReservationTest.php
+++ tests/Feature/BookReservationTest.php
@@ -4,14 +4,16 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Book;
 
 class BookReservationTest extends TestCase
 {
-   /** @test **/
+    use RefreshDatabase;
+   /** @test  */
    public function a_book_can_be_add_to_library() {
 
        $this->withoutExceptionHandling();
-       
+
         $response = $this->post('/books', [
                         'title' => "Cool Book Title",
                         'author' => "ibrahem",
@@ -20,4 +22,53 @@ class BookReservationTest extends TestCase
         $response->assertOk();
        $this->assertCount(1, Book::all());
    }
+
+
+    /** @test  */
+    public function a_title_is_required() {
+
+        $this->withoutExceptionHandling();
+ 
+         $response = $this->post('/books', [
+                         'title' => "",
+                         'author' => "ibrahem",
+                     ]);
+ 
+         $response->assertSessionHasErrors('title');
+     }
+
+
+     /** @test  */
+    public function a_author_is_required() {
+
+        $this->withoutExceptionHandling();
+ 
+         $response = $this->post('/books', [
+                         'title' => "Cool Book Title",
+                         'author' => " ",
+                     ]);
+ 
+         $response->assertSessionHasErrors('author');
+     }
+
+
+     /** @test  */
+   public function a_book_can_be_update_to_library() {
+
+        $this->withoutExceptionHandling();
+
+        $this->post('/books', [
+                'title' => "Cool Book Title",
+                'author' => "ibrahem",
+        ]);
+
+        $book = Book::first();
+        $this->patch('/books/' . $book->id, [
+            'title' => "new title",
+            'author' => "ibrahem",
+        ]);
+
+        $this->assertEquals('new title', Book::first()->title);
+        $this->assertEquals('ibrahem', Book::first()->author);
+}
 }
